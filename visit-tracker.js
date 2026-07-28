12a0418d39262d578d9eec32bc8477ce8faa3ddf
kutaiba290012 <kutaiba_290012@svuonline.org>
2026-07-28 14:35:36 +0300

    fix visitor sources: admin no longer uses redundant query, tracker uses existing app fallback

--- visit-tracker.js
+++ visit-tracker.js
@@ -19,14 +19,19 @@
             else if (r.indexOf('whatsapp') !== -1) src = 'واتساب';
             else src = 'رابط خارجي';
         }
-        var app = firebase.initializeApp({
-            apiKey: "AIzaSyBCP30snA8NGU5PDk6m4Vt_fvYXcxSvem8",
-            authDomain: "summ3a-3fe32.firebaseapp.com",
-            projectId: "summ3a-3fe32",
-            storageBucket: "summ3a-3fe32.firebasestorage.app",
-            messagingSenderId: "587094312862",
-            appId: "1:587094312862:web:ed31e9cf4e97af846ef88f"
-        }, 'tracker');
+        try {
+            var app = firebase.initializeApp({
+                apiKey: "AIzaSyBCP30snA8NGU5PDk6m4Vt_fvYXcxSvem8",
+                authDomain: "summ3a-3fe32.firebaseapp.com",
+                projectId: "summ3a-3fe32",
+                storageBucket: "summ3a-3fe32.firebasestorage.app",
+                messagingSenderId: "587094312862",
+                appId: "1:587094312862:web:ed31e9cf4e97af846ef88f"
+            }, 'tracker');
+        } catch(e2) {
+            // App already exists, use it
+            app = firebase.app('tracker');
+        }
         var db = firebase.firestore(app);
         var ref = db.collection('visitor_sources').doc(today);
         ref.get().then(function(doc) {
@@ -34,7 +39,7 @@
             var count = (data.count || 0) + 1;
             var sources = data.sources || {};
             sources[src] = (sources[src] || 0) + 1;
-            return ref.set({ count: count, sources: sources }, { merge: true });
+            return ref.set({ count: count, sources: sources });
         }).then(function() {
             localStorage.setItem(key, '1');
         }).catch(function(){});
