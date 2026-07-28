775a4765dba921c2f3921c4dc9409ea2e6c042fa
kutaiba290012 <kutaiba_290012@svuonline.org>
2026-07-28 14:22:31 +0300

    add visitor source tracking (Facebook, Instagram, etc.) + admin display

--- visit-tracker.js
+++ visit-tracker.js
@@ -4,6 +4,21 @@
     if (localStorage.getItem(key)) return;
     try {
         if (typeof firebase === 'undefined') return;
+        var src = 'مباشر';
+        var params = new URLSearchParams(window.location.search);
+        if (params.get('utm_source')) src = params.get('utm_source');
+        else if (params.get('fbclid')) src = 'فيس بوك';
+        else if (params.get('igshid')) src = 'انستغرام';
+        else if (document.referrer) {
+            var r = document.referrer.toLowerCase();
+            if (r.indexOf('facebook') !== -1 || r.indexOf('fb.') !== -1) src = 'فيس بوك';
+            else if (r.indexOf('instagram') !== -1) src = 'انستغرام';
+            else if (r.indexOf('google') !== -1 || r.indexOf('search') !== -1) src = 'جوجل';
+            else if (r.indexOf('tiktok') !== -1) src = 'تيك توك';
+            else if (r.indexOf('twitter') !== -1 || r.indexOf('x.com') !== -1) src = 'X';
+            else if (r.indexOf('whatsapp') !== -1) src = 'واتساب';
+            else src = 'رابط خارجي';
+        }
         var app = firebase.initializeApp({
             apiKey: "AIzaSyBCP30snA8NGU5PDk6m4Vt_fvYXcxSvem8",
             authDomain: "summ3a-3fe32.firebaseapp.com",
@@ -16,6 +31,15 @@
         db.collection('visitors').doc(today).set({
             count: firebase.firestore.FieldValue.increment(1)
         }, { merge: true }).then(function() {
+            localStorage.setItem(key + '_src', src);
+            return db.collection('visitor_sources').doc(today).set({
+                count: firebase.firestore.FieldValue.increment(1)
+            }, { merge: true });
+        }).then(function() {
+            return db.collection('visitor_sources').doc(today).update({
+                ['sources.' + src]: firebase.firestore.FieldValue.increment(1)
+            });
+        }).then(function() {
             localStorage.setItem(key, '1');
         }).catch(function(){});
     } catch(e) {}
